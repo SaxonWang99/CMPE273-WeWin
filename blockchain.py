6f3ae379506c3287f6cb63af0a6e771a1a43d808
Tin <tinchale@gmail.com>
2018-04-28 14:44:18 -0700

    valid_trans

--- blockchain.py
+++ blockchain.py
@@ -29,6 +29,33 @@ class Blockchain:
         else:
             raise ValueError('Invalid URL')
 
+    def valid_trans(self, chain):
+        """
+        Determine if a given blockchain is valid
+        :param chain: A blockchain
+        :return: True if valid, False if not
+        """
+
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-----------\n")
+            # Check that the hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # Check that the Proof of Work is correct
+            if not self.valid_proof(last_block['proof'], block['proof'], last_block['previous_hash']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
 
     def valid_chain(self, chain):
         """
